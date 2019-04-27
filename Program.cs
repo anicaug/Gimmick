7b5600eedb2f9cd5ac0089abf1699ebe056bfdfe
anicaug <35672133+anicaug@users.noreply.github.com>
2019-04-27 00:16:13 -0500

    Second commit. File upload.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gimmick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's tell a story about strings.");
+            Console.WriteLine("string anicaug = null;");
+            Console.WriteLine("bool t1 = string.IsNullOrEmpty(anicaug);");
+            string anicaug = null;
+            bool t1 = string.IsNullOrEmpty(anicaug);
+            Console.WriteLine("Run command for boolean."); 
+            Console.WriteLine(t1);
+        }
+    }
+}
